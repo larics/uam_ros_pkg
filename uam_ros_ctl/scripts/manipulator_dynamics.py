b120491182bf7acdbdbd0a5b93135764518f47a2
Filip Zoric <fzoric8@gmail.com>
2025-02-26 17:09:35 +0100

    Added some renaming

--- uam_ros_ctl/scripts/manipulator_dynamics.py
+++ uam_ros_ctl/scripts/manipulator_dynamics.py
@@ -2,11 +2,11 @@ import rospy
 import numpy as np
 from sensor_msgs.msg import JointState
 from std_msgs.msg import Float64MultiArray
+import tf
 
 class ArmDynamicsNode:
     def __init__(self):
         rospy.init_node('robot_dynamics_node', anonymous=True, log_level=rospy.DEBUG)
-        
         # Subscriber to get joint states (positions, velocities)
         self.joint_state_sub = rospy.Subscriber(
             '/arm_joint_states', JointState, self.joint_state_callback)
@@ -21,7 +21,19 @@ class ArmDynamicsNode:
         l4 = 0.1
         self.l = [l1, l2, l3, l4]
         self.reciv_jnt = False
-    
+
+        self.tf_listener = tf.TransformListener()
+
+    def lookup_transform(self, target_frame, source_frame):
+        """Lookup transform between target and source frames."""
+        try:
+            self.tf_listener.waitForTransform(target_frame, source_frame, rospy.Time(0), rospy.Duration(1.0))
+            (trans, rot) = self.tf_listener.lookupTransform(target_frame, source_frame, rospy.Time(0))
+            return np.array(trans), np.array(rot)
+        except (tf.Exception, tf.LookupException, tf.ConnectivityException):
+            rospy.logwarn(f"Could not lookup transform from {source_frame} to {target_frame}")
+            return None, None
+
     def joint_state_callback(self, msg):
         self.reciv_jnt = True
         """Callback function to update joint state values."""
@@ -42,10 +54,9 @@ class ArmDynamicsNode:
                 v[i] = np.array([0, 0, 0])
             else:
                 w[i] = w[i-1] + np.array([0, 0, self.q_v[i]])
-                v[i] = v[i-1] + np.cross(w[i], np.array([self.l[i-1], 0, 0]))
-        rospy.logdebug(f"Current w: {w}")
-        rospy.logdebug(f"Current v: {v}")
-
+                v[i] = v[i-1] + np.cross(w[i], np.array(self.dS[i-1]))
+        #rospy.logdebug(f"Current w: {w}")
+        #rospy.logdebug(f"Current v: {v}")
         return w, v
     
     def back_NE(): 
@@ -57,6 +68,13 @@ class ArmDynamicsNode:
         while not rospy.is_shutdown():
             self.forward_NE()
             self.rate.sleep()
+            p1, R1 = self.lookup_transform('link1', 'base')
+            p2, R2 = self.lookup_transform('link2', 'link1')
+            p3, R3 = self.lookup_transform('link3', 'link2')
+            p4, R4 = self.lookup_transform('link4', 'link3')
+            p5, R5 = self.lookup_transform('link5', 'link4')
+            self.dS = np.round([p2 - p1, p3 - p2, p4 - p3, p5 - p4], 3)
+            print(self.dS)
 
 if __name__ == '__main__':
     aD = ArmDynamicsNode()
