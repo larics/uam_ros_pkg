7fb3bc42eb06bbe567fae8fd1d1b46b8a031ad58
Filip Zoric <fzoric8@gmail.com>
2025-02-26 18:47:22 +0100

    Added init manipulator dynamics

--- uam_ros_ctl/scripts/manipulator_dynamics.py
+++ uam_ros_ctl/scripts/manipulator_dynamics.py
@@ -1,5 +1,6 @@
 import rospy
 import numpy as np
+from scipy.spatial.transform import Rotation 
 from sensor_msgs.msg import JointState
 from std_msgs.msg import Float64MultiArray
 import tf
@@ -39,22 +40,24 @@ class ArmDynamicsNode:
         """Callback function to update joint state values."""
         self.joint_positions = msg.position
         self.joint_velocities = msg.velocity
+        self.joint_accelerations = msg.accelerations
         self.q_p = np.array(self.joint_positions)
         self.q_v = np.array(self.joint_velocities)
+        # TODO: Add acceleration calculation
     
-    def forward_NE(self):
+    def forward_NE(self, dS, R):
         """Forward Newton-Euler calculations."""
         n = len(self.q_p)
         w = np.zeros((n, 3))  # Angular velocity
         v = np.zeros((n, 3))  # Linear velocity
-        
+        # TODO: Add implementation of the linear and angular accs
         for i in range(n):
             if i == 0:
                 w[i] = np.array([0, 0, self.q_v[i]])
                 v[i] = np.array([0, 0, 0])
             else:
-                w[i] = w[i-1] + np.array([0, 0, self.q_v[i]])
-                v[i] = v[i-1] + np.cross(w[i], np.array(self.dS[i-1]))
+                w[i] = w[i-1] + np.dot(self.q_v[i], R[i-1][:, 2])
+                v[i] = v[i-1] + np.cross(w[i], np.array(dS[i-1])) + np.dot(self.q_v[i], R[i-1][:, 2])
         #rospy.logdebug(f"Current w: {w}")
         #rospy.logdebug(f"Current v: {v}")
         return w, v
@@ -66,15 +69,18 @@ class ArmDynamicsNode:
 
     def run(self):
         while not rospy.is_shutdown():
-            self.forward_NE()
             self.rate.sleep()
             p1, R1 = self.lookup_transform('link1', 'base')
             p2, R2 = self.lookup_transform('link2', 'link1')
             p3, R3 = self.lookup_transform('link3', 'link2')
             p4, R4 = self.lookup_transform('link4', 'link3')
             p5, R5 = self.lookup_transform('link5', 'link4')
-            self.dS = np.round([p2 - p1, p3 - p2, p4 - p3, p5 - p4], 3)
-            print(self.dS)
+            dS = np.round([p2 - p1, p3 - p2, p4 - p3, p5 - p4], 3)
+            Rs = [R1, R2, R3, R4, R5]
+            R_ = [Rotation.from_quat((R_[0], R_[1], R_[2], R_[3])).as_matrix() for R_ in Rs]
+            self.forward_NE(dS, R_)
+
+            #print(self.dS)
 
 if __name__ == '__main__':
     aD = ArmDynamicsNode()
