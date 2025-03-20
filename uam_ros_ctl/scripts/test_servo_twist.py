0a75751f0d75c23553ba8efc031c6eb3a932d392
Filip Zoric <fzoric8@gmail.com>
2025-03-20 18:09:17 +0100

    minor changes for the servo twist script

--- uam_ros_ctl/scripts/test_servo_twist.py
+++ uam_ros_ctl/scripts/test_servo_twist.py
@@ -15,7 +15,7 @@ def publish_twist():
         current_time = rospy.Time.now()
         elapsed_time = current_time.to_sec() - start_time
 
-        phase = int(elapsed_time // 10) % 3  # 0: x, 1: y, 2: z
+        phase = int(elapsed_time // 10) % 6  # 0: x, 1: y, 2: z
 
         twist_msg = TwistStamped()
         twist_msg.header.stamp = current_time
@@ -27,8 +27,14 @@ def publish_twist():
             twist_msg.twist.linear.x = sinusoid_value
         elif phase == 1:
             twist_msg.twist.linear.y = sinusoid_value
-        else:
-            twist_msg.twist.linear.z = sinusoid_value * 0.5
+        elif phase == 2:
+            twist_msg.twist.linear.z = sinusoid_value
+        elif phase == 3:
+            twist_msg.twist.angular.x = sinusoid_value
+        elif phase == 4:
+            twist_msg.twist.angular.y = sinusoid_value
+        elif phase == 5:
+            twist_msg.twist.angular.z = sinusoid_value  
 
         pub.publish(twist_msg)
         rospy.loginfo(f'Publishing TwistStamped: {twist_msg}')
